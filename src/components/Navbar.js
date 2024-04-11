a51b4b02c1bba18a5fae1202e938d63b5efc0096
harshagudem <157737492+harshagudem@users.noreply.github.com>
2024-04-11 06:06:44 +0000

    color changes on hover of nav components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,22 +10,24 @@ function Navbar() {
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
+
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img src={Logo} alt="logo" />
+        <h1 className="heading">Pedro's Pizzeria</h1>
         <div className="hiddenLinks">
-          <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/contact"> Contact </Link>
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
       <div className="rightSide">
-        <Link to="/"> Home </Link>
-        <Link to="/menu"> Menu </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/contact"> Contact </Link>
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
         <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
